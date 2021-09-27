96f37f8475faffbd1eacbf8c4738b163e42f763f
laza <lazazael@gmail.com>
2021-09-27 20:31:45 +0200

    setup session for remoteIP

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -33,6 +33,10 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	//_, _= fmt.Fprintf(w,"This is the home page.")
 
+	//storing the remoteIP on call
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -41,6 +45,10 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//perform logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "LoLo again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	//send data to the template
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
